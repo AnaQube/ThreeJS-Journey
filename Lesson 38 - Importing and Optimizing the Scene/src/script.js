82a78ee8dfd925dbfba93d2b41594d52326c081a
Justin Chen <justin.chen@stonybrook.edu>
2023-01-21 21:47:25 -0600

    Add lesson 38/39 and updated 36

--- Lesson 38 - Importing and Optimizing the Scene/src/script.js
+++ Lesson 38 - Importing and Optimizing the Scene/src/script.js
@@ -4,6 +4,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import firefliesVertexShader from './shaders/fireflies/vertex.glsl'
+import firefliesFragShader from './shaders/fireflies/frag.glsl'
+import portalVertexShader from './shaders/portal/vertex.glsl'
+import portalFragShader from './shaders/portal/frag.glsl'
+
 
 /**
  * 1. This lesson covers adding the previous lesson's model to ThreeJS and optimizing it.
@@ -17,7 +22,20 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
  * 7. Go into blender, dupe everything besides the emissions, put those into a new collection, then merge them (ctrl J). Blender also does multiple materials for a single object as well (like multi materials for one avatar)
  * 8. You can also remove the materials from the merged object but it's up to you. If you need to rebake, you still got the original scene. If you want multi materials, check export materials.
  * 9. Now we're at 16 draw calls. Remember to export those emissions as part of the model.
- * 10. Since we're now at 1 object for the entire scene, we can now do .find() to get the one object instead of traversing.
+ * 10. Since we're now at 1 object for the entire scene, we can now do .find() to get the one object instead of traversing all the children of the scene to apply the material.
+ * 11. Continuing to lesson 39... we need to add fireflies, portal shader, and just some details.
+ * 12. Add a debug color for the background
+ * 13. Create particles for the fireflies: Go look at the particles AND shaders lessons since the shaders deals with particles as well.
+ *     Couple of reminders: pixel ratio affects shaders and particularly particle size. Need to implement attenuation in shader manually.
+ * 14. A little tip on shader randomness: you can use textures for noise, you can use attributes to send a random for every particle, but those are bad for performance (it's ok at small values)
+ *     Instead, you can use the position on the x or z axis to add a little bit of randomness (or other attributes that are already random).
+ * 15. By the way, points already has a UV vec2 called gl_PointCoord. Everything else needs the varying uv sent to frag shader.
+ * 16. If the portal is a flat color when you set the uv coordinates to your fragColor, then it isn't unwrapped. Go back in blender and unwrap it again.
+ * 17. To get better perlin noise, you can run it twice. Once, on the original UV coordinates, then take those coordinates and run it again. Apply different time variables cnoise(1st and 2nd parameters are UV, 3rd is time)
+ * 18. We want an outer glow on the portal to match the lighting on the bricks of the portal. Do not use step as we want the glow part and not a sharp edge. Just multiply the distance function by 5.0
+ *     You will also notice there is black on the edges cause you're adding the original strength still. Use a step function to filter that out but also add the original strength to get a bit of blur.
+ *     IMPORTANT: There is also a bug now since you don't get the correct colors when setting them. This is the strength > 1.0 bug. Clamp it to 0.0, 1.0.
+ *     ALWAYS CLAMP YOUR STRENGTH IF YOU'RE > 1.0 FOR COLORS.
  */
 
 /**
@@ -31,6 +49,7 @@ spector.displayUI()
  * Base
  */
 // Debug
+const debugObject = {}
 const gui = new dat.GUI({
     width: 400
 })
@@ -67,7 +86,26 @@ bakedTexture.encoding = THREE.sRGBEncoding
  */
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
 const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xCEA859 })
-const portalLightMaterial = new THREE.MeshBasicMaterial({ color: 0x5F73FF })
+
+debugObject.portalColorStart = '#ff0000'
+debugObject.portalColorEnd = '#0000ff'
+gui.addColor(debugObject, 'portalColorStart').onChange(() => {
+    portalLightMaterial.uniforms.uColorStart.value.set(debugObject.portalColorStart)
+})
+gui.addColor(debugObject, 'portalColorEnd').onChange(() => {
+    portalLightMaterial.uniforms.uColorEnd.value.set(debugObject.portalColorEnd)
+})
+const portalLightMaterial = new THREE.ShaderMaterial({
+    vertexShader: portalVertexShader,
+    fragmentShader: portalFragShader,
+    uniforms: {
+        uTime: { value: 0.0 },
+        uColorStart: { value: new THREE.Color(debugObject.portalColorStart) },
+        uColorEnd: { value: new THREE.Color(debugObject.portalColorEnd) }
+    }
+})
+
+
 
 /**
  * Model
@@ -97,6 +135,48 @@ gltfLoader.load('portal.glb', (gltf) => {
 })
 
 /**
+ * Fireflies - Go review the particles lesson
+ */
+const firefliesGeometry = new THREE.BufferGeometry()
+const firefliesCount = 30
+const positionArray = new Float32Array(firefliesCount * 3)
+const scaleArray = new Float32Array(firefliesCount)
+
+for (let i = 0; i < firefliesCount; i++) {
+    positionArray[i * 3] = (Math.random() - 0.5) * 4
+    positionArray[i * 3 + 1] = Math.random() * 1.5
+    positionArray[i * 3 + 2] = (Math.random() - 0.5) * 4
+
+    scaleArray[i] = Math.random()
+}
+
+firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+firefliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 1))
+
+// Material
+// const firefliesMaterial = new THREE.PointsMaterial({
+//     size: 0.1,
+//     sizeAttenuation: true
+// })
+const firefliesMaterial = new THREE.ShaderMaterial({
+    vertexShader: firefliesVertexShader,
+    fragmentShader: firefliesFragShader,
+    uniforms: {
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 100 },
+        uTime: { value: 0 }
+    },
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    depthWrite: false
+})
+gui.add(firefliesMaterial.uniforms.uSize, 'value', 0, 500, 1)
+
+// Points
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial)
+scene.add(fireflies)
+
+/**
  * Sizes
  */
 const sizes = {
@@ -117,6 +197,7 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    firefliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
 })
 
 /**
@@ -144,6 +225,12 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.outputEncoding = THREE.sRGBEncoding
 
+debugObject.clearColor = '#122030'
+gui.addColor(debugObject, 'clearColor').onChange(() => {
+    renderer.setClearColor(debugObject.clearColor)
+})
+renderer.setClearColor(debugObject.clearColor)
+
 /**
  * Animate
  */
@@ -152,7 +239,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+    
+    firefliesMaterial.uniforms.uTime.value = elapsedTime
+    portalLightMaterial.uniforms.uTime.value = elapsedTime
     // Update controls
     controls.update()
 
